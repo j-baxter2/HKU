38ec8fe0e5944ee01049271fd1c64197c8d97e8e
Jamie Baxter <jamiebaxter2@outlook.com>
2024-07-24 20:25:48 +0100

    fixed main menu

--- src/views/menu.py
+++ src/views/menu.py
@@ -14,20 +14,33 @@ class MenuView(arcade.View):
 
         new_game_button = arcade.gui.UITextureButton(texture=new_game_texture)
         self.v_box.add(new_game_button.with_space_around(bottom=20))
-        new_game_button.on_click = self.on_click_new_game
 
         settings_button = arcade.gui.UITextureButton(texture=settings_texture)
         self.v_box.add(settings_button.with_space_around(bottom=20))
-        settings_button.on_click = self.on_click_settings
 
         quit_button = arcade.gui.UITextureButton(texture=quit_texture)
         self.v_box.add(quit_button.with_space_around(bottom=20))
-        quit_button.on_click = self.on_click_quit
+
         self.manager.add(
             arcade.gui.UIAnchorWidget(
                 anchor_x="center_x", anchor_y="center_y", child=self.v_box
             )
         )
+
+
+        @new_game_button.event("on_click")
+        def on_click_new_game(event):
+            game_view = GameView()
+            game_view.setup()
+            self.window.show_view(game_view)
+
+        @settings_button.event("on_click")
+        def on_click_settings(self, event):
+            pass
+
+        @settings_button.event("on_click")
+        def on_click_quit(self, event):
+            arcade.close_window()
 
     def on_update(self,delta_time):
         self.manager.on_update(delta_time)
@@ -36,17 +49,9 @@ class MenuView(arcade.View):
         arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
         self.manager.enable()
 
+    def on_hide_view(self):
+        self.manager.disable()
+
     def on_draw(self):
         self.clear()
         self.manager.draw()
-
-    def on_click_new_game(self, event):
-        game_view = GameView()
-        game_view.setup()
-        self.window.show_view(game_view)
-
-    def on_click_settings(self, event):
-        pass
-
-    def on_click_quit(self, event):
-        arcade.close_window()
