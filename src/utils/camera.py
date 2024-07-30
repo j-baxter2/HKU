f766333c102ac4e8bb97b3c7d4e80ece70e97f9f
Jamie Baxter <jamiebaxter2@outlook.com>
2024-07-30 06:14:43 +0100

    renaming camera object to camera2d

--- src/utils/camera.py
+++ src/utils/camera.py
@@ -1,6 +1,6 @@
 import arcade
 from pyglet.math import Vec2
 
-class HKUCamera(arcade.Camera):
+class HKUCamera(arcade.Camera2D):
     def __init__(self, viewport_width, viewport_height):
         super().__init__(viewport_width, viewport_height)
