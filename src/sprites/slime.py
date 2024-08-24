d393c948593ea9a96913d64cc1f6a84d14410d79
Jamie Baxter <jamiebaxter2@outlook.com>
2024-08-24 18:09:58 +0100

    added option for slime to be non decaying

--- src/sprites/slime.py
+++ src/sprites/slime.py
@@ -5,12 +5,13 @@ from src.data.constants import M, DELTA_TIME, SOUND_EFFECT_VOL
 from src.utils.sound import play_sound
 
 class Slime(arcade.Sprite):
-    def __init__(self, scene: arcade.Scene = None, filename: str = None, scale: float = 1, image_x: float = 0, image_y: float = 0, image_width: float = 0, image_height: float = 0, center_x: float = 0, center_y: float = 0, repeat_count_x: int = 1, repeat_count_y: int = 1, flipped_horizontally: bool = False, flipped_vertically: bool = False, flipped_diagonally: bool = False, hit_box_algorithm: str = "Simple", hit_box_detail: float = 4.5, texture: arcade.Texture = None, angle: float = 0):
+    def __init__(self, scene: arcade.Scene = None, finite: bool = False, filename: str = None, scale: float = 1, image_x: float = 0, image_y: float = 0, image_width: float = 0, image_height: float = 0, center_x: float = 0, center_y: float = 0, repeat_count_x: int = 1, repeat_count_y: int = 1, flipped_horizontally: bool = False, flipped_vertically: bool = False, flipped_diagonally: bool = False, hit_box_algorithm: str = "Simple", hit_box_detail: float = 4.5, texture: arcade.Texture = None, angle: float = 0):
         super().__init__(filename, scale, image_x, image_y, image_width, image_height, center_x, center_y, repeat_count_x, repeat_count_y, flipped_horizontally, flipped_vertically, flipped_diagonally, hit_box_algorithm, hit_box_detail, texture, angle)
         self.scene = scene
         self.player = self.scene.get_sprite_list("Player")[0]
+        self.finite = finite
         self.timer = 0
-        self.lifetime = 5
+        self.lifetime = 5 if finite else None
 
         self.affecting = False
         self.effect_timer = 0
@@ -22,10 +23,11 @@ class Slime(arcade.Sprite):
         if not self.affecting:
             if arcade.check_for_collision(self, self.player):
                 self.affecting = True
-            if self.timer >= self.lifetime:
-                self.rescale_relative_to_point(point=[self.center_x, self.center_y], factor=0.8)
-                if self.height < 2:
-                    self.kill()
+            if self.finite:
+                if self.timer >= self.lifetime:
+                    self.rescale_relative_to_point(point=[self.center_x, self.center_y], factor=0.8)
+                    if self.height < 2:
+                        self.kill()
         else:
             self.effect_timer += DELTA_TIME
             component = min(max(0,255*(self.effect_timer/self.effect_time)),255)
